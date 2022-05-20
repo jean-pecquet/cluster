44186cc101a919206bac29f145cb3ab1c66a78fd
jean-pecquet <81634013+jean-pecquet@users.noreply.github.com>
2022-05-20 15:03:31 +0200

    Update example.cpp

--- out/example.cpp
+++ out/example.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main (int argc, char **argv){
 
 // generaterandomfibers: straightCircle
-cout << endl << "Creating a 200^3 domain with non-intersecting random fibers having a circular cross section, slight preference for angle in XY:"<< endl;
+cout << endl << "Creating a 100^3 domain with non-intersecting random fibers having a circular cross section, slight preference for angle in XY:"<< endl;
 
 // Creates an empty workspace
 puma::Workspace ws(1e-6, false);
