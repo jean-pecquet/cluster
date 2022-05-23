685e14cf6f173ef45b9830ead6755b5bdfe300cd
jean-pecquet <81634013+jean-pecquet@users.noreply.github.com>
2022-05-23 09:11:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pumapy as puma
 
-ws_fibers = puma.generate_random_fibers((100,100,100), 5, None, 0.89, 90, 20, 400)
+ws_fibers = puma.generate_random_fibers((100, 100, 100), 5, None, 0.89, 20, 90, 400)
 
 cond_map = puma.IsotropicConductivityMap()
-cond_map.add_material((0, 89), 0.0257) # conductivity of the air
-cond_map.add_material((90, 255), 12) # conductivity of carbon fibres
+cond_map.add_material((0, 0), 0.0257) # conductivity of the air
+cond_map.add_material((1, ws_fibers.max()), 12) # conductivity of carbon fibres
 
 k_eff_z, T_z, q_z = puma.compute_thermal_conductivity(ws_fibers, cond_map, 'z', 's', tolerance=1e-3, solver_type='cg')
