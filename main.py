5301b21e2030b72b5bc5d390cfc7c62c36be1ac8
jean-pecquet <81634013+jean-pecquet@users.noreply.github.com>
2022-05-23 08:43:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import pumapy as puma
-import numpy as np
-import time
 
 size = (100, 100, 100)
 r = 5
@@ -12,8 +10,6 @@ cond_map = puma.IsotropicConductivityMap()
 cond_map.add_material((0, 1), 0.0257) # conductivity of the air
 cond_map.add_material((2, ws_fibers.max()), 12) # conductivity of carbon fibres
 
-start_time = time.time()
 k_eff_z = puma.compute_thermal_conductivity(ws_fibers, cond_map, 'z', 's')[0]
-duration = time.time() - start_time
 
 print(duration)
